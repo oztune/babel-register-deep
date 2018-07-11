6cca0f25525ef60a54ccd6059f2b25eb42dc3869
Oz <oztune@users.noreply.github.com>
2018-07-11 17:14:24 -0400

    Updating to babel 7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('babel-register')({	ignore: require('./babel-ignore') })
+require('@babel/register')({
+	// NOTE: I tried moving this require to the
+	// top of the file, but everything stopped working.
+	// Keeping it inline for now.
+	ignore: [require('./babel-ignore')],
+	// We need this so that babel will use the .babelrc (or package.json::"babel")
+	// closest to the file it's transpiling.
+	babelrcRoots: true
+})
