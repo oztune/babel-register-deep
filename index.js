e9ebc45e3870d780fc723fd95c8059b05e55e34d
Oz <oztune@users.noreply.github.com>
2019-02-11 16:01:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('@babel/register')({
 	// top of the file, but everything stopped working.
 	// Keeping it inline for now.
 	ignore: [require('./babel-ignore')],
+	extensions: ['.js', '.ts', '.tsx'],
 	// We need this so that babel will use the .babelrc (or package.json::"babel")
 	// closest to the file it's transpiling.
 	babelrcRoots: true
